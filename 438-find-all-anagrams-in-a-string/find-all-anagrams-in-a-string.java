0d964c6bdd59bd98c2c4b6bcc67e06fa2ad13d22
Nivetha-codes <nivethaksridhar@gmail.com>
2026-04-23 16:40:29 +0530

    Time: 10 ms (83.78%) | Memory: 46.5 MB (95.92%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,32 +1,50 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        
+
         int window = p.length();
         int[] pArr = new int[26];
+        int[] windowArr = new int[26];
         List<Integer> res = new ArrayList<>();
 
-        for(char c: p.toCharArray()){
-            pArr[c-'a']++;
+        for (char c : p.toCharArray()) {
+            pArr[c - 'a']++;
         }
 
+        // this approac is O(p * k) - recounting for every char in s
+
+        /*for (int i = 0; i < s.length(); i++) {
         
-        for(int i=0;i<s.length();i++){
-            
             int right = i;
             int tempWindow = window;
             int[] tempArr = new int[26];
-
-            if(s.length()-i >= window){
-                while(tempWindow > 0){
+        
+            if (s.length() - i >= window) {
+                while (tempWindow > 0) {
                     char c = s.charAt(right);
-                    tempArr[c-'a']++;
+                    tempArr[c - 'a']++;
                     tempWindow--;
                     right++;
                 }
-
-                if(Arrays.equals(pArr,tempArr)){
+        
+                if (Arrays.equals(pArr, tempArr)) {
                     res.add(i);
                 }
+            }
+        }
+        */
+
+        //optimised - O (n) approach
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+            windowArr[c - 'a']++;
+
+            if (i >= window) {
+                windowArr[s.charAt(i - window) - 'a']--;
+            }
+
+            if (Arrays.equals(pArr, windowArr)) {
+                res.add(i - window + 1);
             }
         }
 
