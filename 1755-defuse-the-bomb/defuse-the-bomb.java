2647eb57c0fea06a797106ff3d897c1ff32bafd4
Nivetha-codes <nivethaksridhar@gmail.com>
2026-04-29 08:42:34 +0530

    Time: 1 ms (63.84%) | Memory: 44.1 MB (13.76%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.java
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -6,10 +6,10 @@ class Solution {
         if (k == 0)
             return res; // all zeros
 
-
         //Brute Force
+        /*
         for (int i = 0; i < n; i++) {
-
+        
             if (k > 0) {
                 for (int j = i + 1; j <= i + k; j++) {
                     res[i] += code[j % n];
@@ -21,8 +21,31 @@ class Solution {
                 }
             }
         }
+        */
+
+        //Sliding window
+        int sum = 0;
+        int left = 0;
+        for (int i = 0; i < n + Math.abs(k); i++) {
+
+            sum += code[i % n];
+            if (i - left > Math.abs(k) - 1) {
+                sum -= code[left % n];
+                left = (left + 1) % n;
+            }
+
+            if (i - left == Math.abs(k) - 1) {
+                if (k > 0) {
+                    res[(left - 1 + n) % n] = sum;
+                }
+                else {
+                    res[(i + 1) % n] = sum;
+                }
+
+            }
+        }
 
         return res;
-
     }
+
 }
