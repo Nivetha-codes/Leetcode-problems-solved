62689269e8d6385f35918fdc748c50bdb9232d3c
Nivetha-codes <nivethaksridhar@gmail.com>
2026-05-26 09:20:10 +0530

    Time: 4 ms (75.80%) | Memory: 45.4 MB (21.73%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,12 +1,29 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
-     Map<Integer,Integer> map = new HashMap<>();
-     int[] res = new int[nums1.length];
-     Arrays.fill(res,-1);
+        Deque<Integer> stack = new ArrayDeque<>();
 
-    
-     for(int i = 0; i< nums1.length; i++){
+        int[] res = new int[nums1.length];
+        Arrays.fill(res, -1);
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            map.put(nums1[i], i);
+        }
+
+        for(int i = 0; i < nums2.length; i++) {
+
+            while(!stack.isEmpty() && stack.peek() < nums2[i] ){
+                res[map.get(stack.pop())] = nums2[i];
+            }
+            if(map.containsKey(nums2[i])){
+                stack.push(nums2[i]);
+            }
+            
+        }
+
+        /* brute force
+         for(int i = 0; i< nums1.length; i++){
         boolean flag = false;
         for(int j = 0; j< nums2.length; j++){
             if(nums2[j] == nums1[i]){
@@ -19,9 +36,10 @@ class Solution {
                 }
             }
         }
-     }
+         }
+         */
 
-     return res;
+        return res;
 
     }
 }
