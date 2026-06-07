366a3c9858adcf0b83dcf86ef53ad956c7d6a742
Nivetha-codes <nivethaksridhar@gmail.com>
2026-06-07 14:35:28 +0530

    Time: 21 ms (77.56%) | Memory: 48.6 MB (11.75%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,43 +1,62 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
 
-       	record Pair<Character,Integer>(Character c, Integer count) {};
-		Deque<Pair<Character,Integer>> stk = new ArrayDeque<>();
+        record Pair<Character, Integer>(Character key, int count) {
+        }
+        ;
+        Deque<Pair<Character, Integer>> stk = new ArrayDeque<>();
 
-		for (int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			
-			if (!stk.isEmpty()) {
-				if (stk.peek().c == c) {
-					
-					if (stk.peek().count >= k - 1) {
-						while (!stk.isEmpty() && stk.peek().c == c) {
-					
-							stk.pop();
-							
-						}
-					} else {
-						stk.push(new Pair<>(c,stk.peek().count+1));
-						
-					}
+        /* TC : O(N*K) -> because of while loop and adding each node with count
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+        
+            if (!stk.isEmpty()) {
+                if (stk.peek().c == c) {
+        
+                    if (stk.peek().count >= k - 1) {
+                        while (!stk.isEmpty() && stk.peek().c == c) {
+        
+                            stk.pop();
+        
+                        }
+                    } else {
+                        stk.push(new Pair<>(c, stk.peek().count + 1));
+        
+                    }
+        
+                } else {
+                    stk.push(new Pair<>(c, 1));
+        
+                }
+            } else {
+                stk.push(new Pair<>(c, 1));
+        
+            }
+        }
+        
+        */
 
-				} else {
-					stk.push(new Pair<>(c,1));
-					
+        for (char c : s.toCharArray()) {
 
-				}
-			} else {
-				stk.push(new Pair<>(c,1));
-			
+            if (!stk.isEmpty() && stk.peek().key == c) {
+                int newCount = stk.pop().count + 1;
+                if (newCount < k) {
+                    stk.push(new Pair(c, newCount));
+                }
+            } else {
+                stk.push(new Pair(c, 1));
+            }
+        }
 
-			}
-		}
+        StringBuilder sb = new StringBuilder();
+        while (!stk.isEmpty()) {
 
-		StringBuilder sb = new StringBuilder();
-		while(!stk.isEmpty()) {
-			sb.append(stk.pop().c);
-		}
+            Pair p = stk.removeLast();
+            for (int i = 0; i < p.count; i++) {
+                sb.append(p.key);
+            }
+        }
 
-		return sb.reverse().toString();
+        return sb.toString();
     }
 }
