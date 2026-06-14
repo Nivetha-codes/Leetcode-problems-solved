1bdcbb9bb2b5fcb1bc8185be71c981fc5f9c3013
Nivetha-codes <nivethaksridhar@gmail.com>
2026-06-14 18:52:39 +0530

    Time: 24 ms (93.19%) | Memory: 59.8 MB (10.03%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,32 @@
+class StockSpanner {
+
+    record Pair<I, T>(I price, T span) {
+    };
+
+    Deque<Pair<Integer, Integer>> dq;
+
+    public StockSpanner() {
+        dq = new ArrayDeque<>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+        if (!dq.isEmpty()) {
+
+            while (!dq.isEmpty() && price >= dq.peek().price) {
+                span += dq.pop().span;
+            }
+            dq.push(new Pair<>(price, span));
+        } else {
+            dq.push(new Pair<>(price, span));
+        }
+
+        return span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
