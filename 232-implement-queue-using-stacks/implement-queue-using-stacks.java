de14b2b457770f9ac5cd3c5cf5c68fab05ba079a
Nivetha-codes <nivethaksridhar@gmail.com>
2026-06-20 19:35:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (25.16%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -0,0 +1,33 @@
+class MyQueue {
+
+    Deque<Integer> stk;
+
+    public MyQueue() {
+         stk = new ArrayDeque<>();
+    }
+    
+    public void push(int x) {
+        stk.addLast(x);
+    }
+    
+    public int pop() {
+        return stk.pop();
+    }
+    
+    public int peek() {
+        return stk.peek();
+    }
+    
+    public boolean empty() {
+        return stk.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
