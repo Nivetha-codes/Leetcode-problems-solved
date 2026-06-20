8b5c1a55f87bcc2431d2b522921c764ddaabb580
Nivetha-codes <nivethaksridhar@gmail.com>
2026-06-20 22:13:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (25.16%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,25 +1,59 @@
+// class MyQueue {
+
+//     Deque<Integer> stk;
+
+//     public MyQueue() {
+//          stk = new ArrayDeque<>();
+//     }
+    
+//     public void push(int x) {
+//         stk.addLast(x);
+//     }
+    
+//     public int pop() {
+//         return stk.pop();
+//     }
+    
+//     public int peek() {
+//         return stk.peek();
+//     }
+    
+//     public boolean empty() {
+//         return stk.isEmpty();
+//     }
+// }
+
 class MyQueue {
 
-    Deque<Integer> stk;
+    List<Integer> list;
+    int left = 0;
+    int right = 0;
+
 
     public MyQueue() {
-         stk = new ArrayDeque<>();
+         list = new ArrayList<>();
     }
     
     public void push(int x) {
-        stk.addLast(x);
+        list.add(right,x);
+        right++;
     }
     
     public int pop() {
-        return stk.pop();
+        int val = list.get(left);
+        left++;
+        return val;
     }
     
     public int peek() {
-        return stk.peek();
+        return list.get(left);
     }
     
     public boolean empty() {
-        return stk.isEmpty();
+        if(left == right){
+            return true;
+        }
+        return false;
     }
 }
 
