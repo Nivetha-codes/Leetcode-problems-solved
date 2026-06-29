419825f09dc04c2a9ed3c68df3a1b2ceddc15c78
Nivetha-codes <nivethaksridhar@gmail.com>
2026-06-29 12:27:42 +0530

    Time: 5 ms (96.87%) | Memory: 45.3 MB (45.67%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -4,38 +4,43 @@ class Solution {
         Deque<Integer> stk = new ArrayDeque<>();
 
         for (String s : tokens) {
-            try {
-                int val = Integer.parseInt(s);
-                stk.push(val);
-            } catch (NumberFormatException e) {
 
-                int right_op = stk.pop();
-                int left_op = stk.pop();
-
-                switch (s) {
-                    case "+":
-                        int add = left_op + right_op;
-                        stk.push(add);
-                        break;
-                    case "-":
-                        int sub = left_op - right_op;
-                        stk.push(sub);
-                        break;
-                    case "*":
-                        int mul = left_op * right_op;
-                        stk.push(mul);
-                        break;
-                    case "/":
-                        int div = left_op / right_op;
-                        stk.push(div);
-                        break;
-                    default:
-                        break;
+            switch (s) {
+                case "+":{
+                    int right_op = stk.pop();
+                    int left_op = stk.pop();
+                    int add = left_op + right_op;
+                    stk.push(add);
+                    break;
                 }
+                case "-":{
+                    int right_op = stk.pop();
+                    int left_op = stk.pop();
+                    int sub = left_op - right_op;
+                    stk.push(sub);
+                    break;
+                }
+                case "*":{
+                    int right_op = stk.pop();
+                    int left_op = stk.pop();
+                    int mul = left_op * right_op;
+                    stk.push(mul);
+                    break;
+                }
+                case "/":
+                {
+                    int right_op = stk.pop();
+                    int left_op = stk.pop();
+                    int div = left_op / right_op;
+                    stk.push(div);
+                    break;
+                }
+                default:
+                    stk.push(Integer.parseInt(s));
             }
         }
 
-       return stk.pop();
+        return stk.pop();
 
     }
 }
