d478081d1b0c909626c1376d4d5f8c4e34d0afe1
Nivetha-codes <nivethaksridhar@gmail.com>
2026-08-04 17:23:43 +0530

    Time: 3 ms (27.61%) | Memory: 43.5 MB (77.23%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -2,7 +2,7 @@ class Solution {
     public String removeDuplicateLetters(String s) {
 
         Deque<Character> stk = new ArrayDeque<>();
-        Map<Integer, Boolean> seen = new HashMap<>();
+        boolean[] seen = new boolean[26];
         int[] lastIndex = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
@@ -13,17 +13,17 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
 
             char c = s.charAt(i);
-            if (seen.containsKey(c - 'a') && seen.get(c - 'a')) {
+            if (seen[c - 'a']) {
                 continue;
             }
 
             while (!stk.isEmpty() && (stk.peek() > c) && (lastIndex[stk.peek() - 'a'] > i)) {
 
-                seen.put(stk.pop() - 'a', false);
+                seen[stk.pop() - 'a'] = false;
             }
 
             stk.push(c);
-            seen.put(c - 'a', true);
+            seen[c - 'a'] =  true;
         }
 
         String res = "";
