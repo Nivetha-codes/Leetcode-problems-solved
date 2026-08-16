cf447393861629afb830851b43f76e338b320f42
Nivetha-codes <nivethaksridhar@gmail.com>
2026-08-16 16:42:53 +0530

    Time: 29 ms (19.93%) | Memory: 47.3 MB (7.37%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+
+        int l = 0;
+        int r = 0;
+        int maxFreq = 0;
+        int maxLen = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        while(r < s.length()){
+
+            map.put(s.charAt(r),map.getOrDefault(s.charAt(r),0) + 1);
+            maxFreq = Math.max(maxFreq, map.get(s.charAt(r)));
+
+            while((r-l+1) - maxFreq > k){ //windowlength - maxchar's freq
+                map.put(s.charAt(l),map.get(s.charAt(l))-1);
+                l++;
+
+            }
+
+            maxLen = Math.max(maxLen , r-l+1);
+            r++;
+
+        }
+
+        return maxLen;
+        
+    }
+}
