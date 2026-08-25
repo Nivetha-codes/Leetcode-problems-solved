62d0cb9ad4880e364874b1f8a6980ea0f3805cb4
Nivetha-codes <nivethaksridhar@gmail.com>
2026-08-25 09:04:43 +0530

    Time: 51 ms (28.20%) | Memory: 50.3 MB (43.07%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -2,6 +2,8 @@ class Solution {
 
     public int helper(int[] nums, int k) {
 
+        //k different integers means -  exactly k no.of integers, hence the formula, exactly k = atmost(k) - atmost(k-1)
+
         HashMap<Integer, Integer> map = new HashMap<>();
         int count = 0;
         int l = 0;
@@ -11,7 +13,7 @@ class Solution {
 
             map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
 
-            while (map.size() >= k + 1) {
+            while (map.size() > k) {
 
                 map.put(nums[l], map.get(nums[l]) - 1);
 
