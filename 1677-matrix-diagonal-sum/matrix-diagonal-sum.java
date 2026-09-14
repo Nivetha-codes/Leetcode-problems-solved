877233f8192d8232e4fb831d86285e06f3e78d34
Nivetha-codes <nivethaksridhar@gmail.com>
2026-09-15 00:01:33 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (87.66%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -5,8 +5,11 @@ class Solution {
             int sum = 0;
             for(int i = 0; i< mat.length; i++){
                 
-                sum += mat[i][i];
+                sum += mat[i][i]; // primary diagonal indexes are 00,11,22,33 etc..
                 
+                //secondary diagonal pattern is i increment by 1 and j decrement by 1 -> can also be written as j + i = n - 1 (03,11,20 etc. n = 4) -> j = n-1-i or i != n-1-i
+
+                // as we are traversing in linear order, when mat length is odd, the mid index common to both pri and sec diagonal is not calculated twice. 
                 if(i != (mat.length - 1 - i)){
                       sum += mat[i][mat.length - 1 - i];
                 }
